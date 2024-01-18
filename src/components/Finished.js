4d5060c6d83a606eca7f864bcc37292f05eaceee
Divya Motghare <divyamotghare9@gmail.com>
2024-01-19 00:47:51 +0530

    rendering the score/percentage and highscore

---
+++ src/components/Finished.js
@@ -0,0 +1,32 @@
+function Finished({ points, maxPossiblePoints, highscore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+
+  if (percentage === 100) emoji = "🔥";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "😃";
+  if (percentage >= 0 && percentage < 50) emoji = "☹️";
+  if (percentage === 0) emoji = "🤦‍♀️";
+
+  return (
+    <div>
+      <p className="result">
+        <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)})
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+
+      <button
+        onClick={() => {
+          dispatch({ type: "reset" });
+        }}
+        className="btn btn-ui"
+      >
+        Restart quiz
+      </button>
+    </div>
+  );
+}
+
+export default Finished;
